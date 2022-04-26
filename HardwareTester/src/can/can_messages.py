35983f8b4fafe8b45be40bc55c890d7871c3a536
The1TrueJoe <jttelaak@hotmail.com>
2022-04-26 18:51:34 -0400

    Add messages and update ui

--- HardwareTester/src/can/can_messages.py
+++ HardwareTester/src/can/can_messages.py
@@ -1,25 +1,42 @@
 import can_util as util
 import cart_module as m
 
-turn_left = ""
-turn_right = ""
-engage_brakes = ""
-disengage_brake = ""
-accelerate = ""
-deccelerate = ""
-stop_accel = ""
-right_signal_on = ""
-right_signal_off = ""
-left_signal_on = ""
-left_signal_off = ""
-tail_on = ""
-tail_off = ""
-head_on = ""
-head_off = ""
-honk = ""
-rear_buzz = ""
-disable = ""
-enable = ""
-forward = ""
-reverse = ""
+# Hardware Module Tester
+# Messages
+#
+# Part of the GSSM Autonomous Golf Cart
+# Written by: Joseph Telaak, class of 2022
 
+# Direction Module: Steering
+turn_left = "(4081) 10 1 12 1 255 0 0 0"
+turn_right = "(4081) 10 1 12 2 255 0 0 0"
+stop_turn = "(4081) 10 1 10 1 0 0 0 0"
+
+# Direction Module: Brakes
+pull_brakes = "(4081) 10 2 12 1 255 0 0 0"
+release_brakes = "(4081) 10 2 12 2 255 0 0 0"
+disable_brakes = "(4081) 10 2 10 1 0 0 0 0"
+
+# Drive Module: Direction Control
+disable = "(4083) 10 10 14 1 0 0 0 0"
+enable = "(4083) 10 10 14 1 0 0 0 0"
+forward = "(4083) 10 13 1 0 0 0 0 0)"
+reverse = "(4083) 10 13 2 0 0 0 0 0"
+
+# Drive Module: Speed Control
+accelerate = "(4083) 11 10 1 0 0 0 0 0"
+deccelerate = "(4083) 11 10 2 0 0 0 0 0"
+stop_accel = "(4083) 11 10 13 0 0 0 0 0"
+
+# Accessory Module: Accessories
+right_signal_on = "(4082) 10 1 1 0 0 0 0 0"
+right_signal_off = "(4082) 10 1 2 0 0 0 0 0"
+left_signal_on = "(4082) 10 2 1 0 0 0 0 0"
+left_signal_off = "(4082) 10 2 2 0 0 0 0 0"
+tail_on = "(4082) 10 4 1 0 0 0 0 0"
+tail_off = "(4082) 10 4 2 0 0 0 0 0"
+head_on = "(4082) 10 3 1 0 0 0 0 0"
+head_off = "(4082) 10 3 2 0 0 0 0 0"
+honk = "(4082) 11 1 0 0 0 0 0 0"
+rear_buzz_on = "(4082) 10 6 1 0 0 0 0 0"
+rear_buzz_off = "(4082) 10 6 1 0 0 0 0 0"
--- HardwareTester/src/tester.py
+++ HardwareTester/src/tester.py
@@ -1,9 +1,23 @@
 import os
-import src.util as util
 
+import src.util as util
+import src.can.can_messages as msg
+from src.can_adapter import CAN_Adapter
+
+# Hardware Module Tester
+# Main
+#
+# Part of the GSSM Autonomous Golf Cart
+# Written by: Joseph Telaak, class of 2022
+
+# CAN Adapter
+can = CAN_Adapter(serial_port="", baud=11520)
+
+# Run
 def run():
     main_menu()
 
+# Clear screen
 def clear():
     # Clear Screen
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -12,37 +26,42 @@ def clear():
     print(util.to_color(util.title, "cyan"))
     print(util.info_block)
 
+# Main menu
 def main_menu():
     clear()
     print("Main Menu: ")
 
     print("""
-        1) Steering
-        2) Brakes
-        3) Acceleration
-        4) Accessories
-        5) Misc
-        6) Run in Manual Mode
+        1) Direction
+        2) Steering
+        3) Brakes
+        4) Acceleration
+        5) Accessories
+        6) Misc
+        7) Run in Manual Mode
     
     """)
 
     option = int(input("Enter Choice: "))
 
     if option == 1:
-        steering()
+        direction()
     elif option == 2:
-        brakes()
+        steering()
     elif option == 3:
-        acclerator()
+        brakes()
     elif option == 4:
-        accessories()
+        acclerator()
     elif option == 5:
-        misc()
+        accessories()
     elif option == 6:
+        misc()
+    elif option == 7:
         manual()
     else:
         main_menu()
 
+# Steering menu
 def steering():
     clear()
     print("Steering Menu: ")
@@ -50,39 +69,74 @@ def steering():
     print("""
         1) Turn Right
         2) Turn Left
-        3) Back
+        3) Stop Turn
+        4) Back
     
     """)
 
     option = int(input("Enter Choice: "))
 
     if option == 1:
-        pass
+        can.write(msg.turn_right)
     elif option == 2:
-        pass
+        can.write(msg.turn_left)
+    elif option == 3:
+        can.write(msg.stop_turn)
     else:
         main_menu()
 
+# Direction Menu
+def direction():
+    clear()
+    print("Direction Menu: ")
+
+    print("""
+        1) Forward
+        2) Reverse
+        3) Enable
+        4) Disable
+        5) Back
+    
+    """)
+
+    option = int(input("Enter Choice: "))
+
+    if option == 1:
+        can.write(msg.forward)
+    elif option == 2:
+        can.write(msg.reverse)
+    elif option == 3:
+        can.write(msg.enable)
+    elif option == 4:
+        can.write(msg.disable)
+    else:
+        main_menu()
+
+# Brakes menu
 def brakes():
     clear()
     print("Brakes Menu: ")
 
     print("""
-        1) Enagage
-        2) Disengage
-        3) Back
+        1) Pull
+        2) Release
+        3) Disable
+        4) Back
     
     """)
 
     option = int(input("Enter Choice: "))
 
     if option == 1:
-        pass
+        can.write(msg.pull_brakes)
     elif option == 2:
-        pass
+        can.write(msg.release_brakes)
+    elif option == 3:
+        can.write(msg.disable_brakes)
     else:
         main_menu()
 
+# Accelerator menu
 def acclerator():
     clear()
     print("Accelerator Menu: ")
@@ -98,14 +152,15 @@ def acclerator():
     option = int(input("Enter Choice: "))
 
     if option == 1:
-        pass
+        can.write(msg.accelerate)
     elif option == 2:
-        pass
+        can.write(msg.deccelerate)
     elif option == 3:
-        pass
+        can.write(msg.stop_accel)
     else:
         main_menu()
 
+# Accessory menu
 def accessories():
     clear()
     print("Accessory Menu: ")
@@ -121,21 +176,39 @@ def accessories():
         8) Right Signal Off
         9) Rear Buzzer On
         10) Rear Buzzer Off
-        11) Back
+        11) Honk
+        12) Back
     
     """)
 
     option = int(input("Enter Choice: "))
 
     if option == 1:
-        pass
+        can.write(msg.head_on)
     elif option == 2:
-        pass
+        can.write(msg.head_off)
     elif option == 3:
-        pass
+        can.write(msg.tail_on)
+    elif option == 4:
+        can.write(msg.tail_off)
+    elif option == 5:
+        can.write(msg.left_signal_on)
+    elif option == 6:
+        can.write(msg.left_signal_off)
+    elif option == 7:
+        can.write(msg.right_signal_on)
+    elif option == 8:
+        can.write(msg.right_signal_off)
+    elif option == 9:
+        can.write(msg.rear_buzz_on)
+    elif option == 10:
+        can.write(msg.rear_buzz_off)
+    elif option == 11:
+        can.write(msg.honk)
     else:
         main_menu()
 
+# Misc menu
 def misc():
     clear()
     print("Misc Menu: ")
@@ -149,15 +222,9 @@ def misc():
 
     if option == 1:
         pass
-    elif option == 2:
-        pass
-    elif option == 3:
-        pass
     else:
         main_menu()
 
+# Manual mode operation
 def manual():
     pass
-
-if __name__ == "__main__":
-    main_menu()
