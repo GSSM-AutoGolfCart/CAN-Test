039cf1a8efc81fe3855684ac2a52c2d92fe298f3
The1TrueJoe <jttelaak@hotmail.com>
2022-05-08 20:09:49 -0400

    Remove vars

--- mon.sh
+++ mon.sh
@@ -4,12 +4,9 @@
 # Part of the GSSM Autonomous Golf Cart
 # Written by: Joseph Telaak, class of 2022
 
-# Serial Port
-$tty_port = "/dev/ttyACM0"
-
 # Print STD Program Header
 python3.7 HardwareTester/src/util.py
 
 # Print serial output
-stty -F $tty_port raw 115200
-cat $tty_port
+stty -F "/dev/ttyACM0" raw 115200
+cat "/dev/ttyACM0"
--- run.sh
+++ run.sh
@@ -4,11 +4,8 @@
 # Part of the GSSM Autonomous Golf Cart
 # Written by: Joseph Telaak, class of 2022
 
-# Serial Port
-$tty_port = "/dev/ttyACM0"
-
 #Update
 #git pull
 
 # Run
-python3.7 HardwareTester $tty_port
+python3.7 HardwareTester "/dev/ttyACM0"
